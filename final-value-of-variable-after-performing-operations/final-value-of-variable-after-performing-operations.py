6512453b7ed4b7cf22a0286a369f9ae0466462a3
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-09-24 23:57:27 +0300

    Time: 48 ms (92.29%), Space: 14.4 MB (15.23%) - LeetHub

--- final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -1,11 +1,3 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        val = 0
-        for i in operations:
-            if '+' in i:
-                val += 1
-            elif '-' in i:
-                val -= 1
-            else:
-                print('Input is not correct')
-        return val
+        return sum([1 if '+' in o else -1 for o in operations])
