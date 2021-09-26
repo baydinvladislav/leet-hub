9dd21b9af49c6c5ac4ecaa2a850f2ffb79eba307
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-09-26 20:00:44 +0300

    Time: 92 ms (96.51%), Space: 15.7 MB (53.57%) - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countComponents(self, n, edges):
+        g = {x: [] for x in range(n)}
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+
+        ret = 0
+        for i in range(n):
+            queue = [i]
+            ret += 1 if i in g else 0
+            for j in queue:
+                if j in g:
+                    queue += g[j]
+                    del g[j]
+
+        return ret
