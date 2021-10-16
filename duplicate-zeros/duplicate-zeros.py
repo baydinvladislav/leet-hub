97e394db382bdb0a98019e0345bcfae5fdf35a97
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-16 20:19:20 +0300

    Time: 60 ms (98.70%), Space: 14.9 MB (61.49%) - LeetHub

--- duplicate-zeros/duplicate-zeros.py
+++ duplicate-zeros/duplicate-zeros.py
@@ -1,30 +1,16 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        """
-        Do not return anything, modify arr in-place instead.
-        """
+        n = len(arr)
+        possible_dups = arr.count(0)
 
-        possible_dups = 0
-        length_ = len(arr) - 1
-
-        for left in range(length_ + 1):
-
-            if left > length_ - possible_dups:
-                break
-
-            if arr[left] == 0:
-                if left == length_ - possible_dups:
-                    arr[length_] = 0
-                    length_ -= 1
-                    break
-                possible_dups += 1
-
-        last = length_ - possible_dups
-
-        for i in range(last, -1, -1):
-            if arr[i] == 0:
-                arr[i + possible_dups] = 0
-                possible_dups -= 1
-                arr[i + possible_dups] = 0
-            else:
+        for i in range(n - 1, -1, -1):
+            if i + possible_dups < n:
                 arr[i + possible_dups] = arr[i]
+
+            if arr[i] == 0:
+                possible_dups -= 1
+
+                if i + possible_dups < n:
+                    arr[i + possible_dups] = 0
+
+        print(arr)
