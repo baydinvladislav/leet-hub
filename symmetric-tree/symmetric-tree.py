a0a33952f7509d7a218e4eeb915f41645e2a234e
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-20 17:35:08 +0300

    Time: 70 ms (5.39%), Space: 14.5 MB (16.71%) - LeetHub

---
+++ symmetric-tree/symmetric-tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def traversal_right(self, root: Optional[TreeNode], array):
+        if root:
+            self.traversal_right(root.right, array)
+            array.append(root.val)
+            self.traversal_right(root.left, array)
+        array.append(None)
+
+    def traversal_left(self, root: Optional[TreeNode], array):
+        if root:
+            self.traversal_left(root.left, array)
+            array.append(root.val)
+            self.traversal_left(root.right, array)
+        array.append(None)
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        left = []
+        right = []
+
+        self.traversal_left(root.left, left)
+        self.traversal_right(root.right, right)
+
+        return True if left == right else False
