cdf6e9772f6cbdd6649181f0a8a381ac381c8b85
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-10-26 17:28:05 +0300

    Time: 32 ms (87.41%), Space: 14.4 MB (17.22%) - LeetHub

--- symmetric-tree/symmetric-tree.py
+++ symmetric-tree/symmetric-tree.py
@@ -1,27 +1,32 @@
 class Solution:
 
-    def _in_order_traversal_left(self, node, left_arr):
-        if node is not None:
-            self._in_order_traversal_left(node.left, left_arr)
-            left_arr.append(node.val)
-            self._in_order_traversal_left(node.right, left_arr)
-        left_arr.append(None)
+    def traversal_left_tree(self, node, values_arr):
 
-    def _in_order_traversal_right(self, node, right_arr):
-        if node is not None:
-            self._in_order_traversal_right(node.right, right_arr)
-            right_arr.append(node.val)
-            self._in_order_traversal_right(node.left, right_arr)
-        right_arr.append(None)
+        if node is None:
+            return
+
+        self.traversal_left_tree(node.left, values_arr)
+        values_arr.append(node.val)
+        self.traversal_left_tree(node.right, values_arr)
+
+        values_arr.append(None)
+
+    def traversal_right_tree(self, node, values_arr):
+
+        if node is None:
+            return
+
+        self.traversal_right_tree(node.right, values_arr)
+        values_arr.append(node.val)
+        self.traversal_right_tree(node.left, values_arr)
+
+        values_arr.append(None)
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         left = []
         right = []
 
-        self._in_order_traversal_left(root.left, left)
-        self._in_order_traversal_right(root.right, right)
+        self.traversal_left_tree(root.left, left)
+        self.traversal_right_tree(root.right, right)
 
-        if left == right:
-            return True
-        else:
-            return False
+        return left == right
