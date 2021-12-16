637224ccad0134950250f333517e183532d9f6eb
Vladislav Baydin <baydinvladislav@yandex.ru>
2021-12-16 13:32:37 +0300

    Time: 52 ms (49.07%), Space: 20.2 MB (6.00%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,10 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        filtered_chars = filter(lambda ch: ch.isalnum(), s)
-        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
-
-        filtered_chars_list = list(lowercase_filtered_chars)
-        reversed_chars_list = filtered_chars_list[::-1]
-
-        return filtered_chars_list == reversed_chars_list
+        filtered_symbols = filter(lambda ch: ch.isalnum(), s)
+        low_case = map(lambda ch: ch.lower(), filtered_symbols)
+        result_list = list(low_case)
+        return result_list == result_list[::-1]
