93754fcdbe49f0d3bca806eded6a2a29595502cf
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-01-16 20:49:23 +0300

    Time: 48 ms (74.93%), Space: 14.6 MB (31.66%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        buffer_1, buffer_2 = {}, {}
+
+        for char in s:
+            buffer_1[char] = buffer_1.get(char, 0) + 1
+
+        for char in t:
+            buffer_2[char] = buffer_2.get(char, 0) + 1
+
+        return buffer_1 == buffer_2
