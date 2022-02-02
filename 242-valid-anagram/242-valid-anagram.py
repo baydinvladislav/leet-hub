de58792d2315878dea9eb11fb8abb787a4eb0b8a
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-02 17:19:31 +0300

    Time: 94 ms (15.33%), Space: 14.5 MB (55.90%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,11 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        buffer_1, buffer_2 = {}, {}
-
+        if len(s) != len(t):
+            return False
+        
+        dict_a, dict_b = {}, {}
         for char in s:
-            buffer_1[char] = buffer_1.get(char, 0) + 1
-
+            dict_a[char] = dict_a.get(char, 0) + 1
+        
         for char in t:
-            buffer_2[char] = buffer_2.get(char, 0) + 1
-
-        return buffer_1 == buffer_2
+            dict_b[char] = dict_b.get(char, 0) + 1
+        
+        return dict_a == dict_b
