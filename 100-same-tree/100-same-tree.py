e5b56c600206fde429f9073943926e8d9015e6f5
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-02 13:27:53 +0300

    Time: 28 ms (27.93%), Space: 13.6 MB (39.17%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -1,12 +1,26 @@
 class Solution(object):
+    # iterative
     def isSameTree(self, p, q):
-        if not p and not q:
+        
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
             return True
-        
-        if not p or not q:
-            return False
-        
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+
+        deq = deque([(p, q), ])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+
+        return True
