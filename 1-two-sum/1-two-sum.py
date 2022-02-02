182bee9d3bb487dbb7d077db24db617754f2a2eb
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-02 16:39:45 +0300

    Time: 110 ms (45.71%), Space: 15.2 MB (61.08%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,13 +2,14 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if not nums:
             return [-1, -1]
-
+        
         buffer = {}
-        for i, num in enumerate(nums):
-            attempt = target - num
-            if attempt not in buffer:
-                buffer[num] = i
-            else:
+        for i in range(len(nums)):
+            buffer[nums[i]] = i
+        
+        for i in range(len(nums)):
+            attempt = target - nums[i]
+            if attempt in buffer and buffer[attempt] != i:
                 return [buffer[attempt], i]
-
         return [-1, -1]
+    
