71a2f0d78266235d4c8fd10efb62e8bc382695fc
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-03 18:33:43 +0300

    Time: 53 ms (22.53%), Space: 13.8 MB (97.68%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,13 +1,17 @@
 class Solution:
     def isValid(self, s):
-        d = {'(': ')', '{': '}', '[': ']'}
+        hash_map = {'(': ')', '[': ']', '{': '}',}
         stack = deque()
-
-        for i in s:
-            if i in d:
-                stack.append(i)
-
-            elif len(stack) == 0 or d[stack.pop()] != i:
-                return False
-
-        return len(stack) == 0
+        
+        for char in s:
+            if char in hash_map:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                
+                last_el = stack.pop()
+                if hash_map[last_el] != char:
+                    return False
+        return not stack
+                
