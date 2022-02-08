f83e990fc66d1bfb974a60c34fbdcff2e81de0a4
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-08 15:44:51 +0300

    Time: 2463 ms (11.98%), Space: 36.2 MB (48.95%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,22 +1,10 @@
 class MedianFinder:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.store = []
-        self.len = len(self.store)
-        
-    def addNum(self, num):
-        """
-        Adds a num into the data structure.
-        :type num: int
-        :rtype: void
-        """
-        # keep a sorted array
-        # use binary search to find insertion index
 
+    def addNum(self, num):
         low = 0
-        high = self.len - 1
+        high = len(self.store) - 1
 
         while low <= high:
             mid = (low + high) // 2
@@ -26,17 +14,10 @@ class MedianFinder:
                 low = mid + 1
 
         self.store.insert(low, num)
-        self.len += 1
 
     def findMedian(self):
-        """
-        Returns the median of current data stream
-        :rtype: float
-        """
-        # as array is always sorted we can always use the same median property
-
-        if self.len % 2 == 0:
-            temp = self.len // 2
+        if len(self.store) % 2 == 0:
+            temp = len(self.store) // 2
             return (self.store[temp] + self.store[temp - 1]) / 2.0
         else:
-            return self.store[self.len // 2]
+            return self.store[len(self.store) // 2]
