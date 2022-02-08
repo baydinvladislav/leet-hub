9de237232267e77c836d209fc2a7364a1d865dc6
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-08 18:24:11 +0300

    Time: 132 ms (87.48%), Space: 15.1 MB (59.83%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -1,33 +1,12 @@
 class Solution:
-    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
-        def dfs(root,subroot):
-            
-            #if both are none
-            if not root and not subroot:
-                return True
-            
-            
-            #if any one is none
-            if not root or not subroot:
-                return False
-            
-            # if node value is not same
-            if root.val!=subroot.val:
-                return False
-            
-            #node vaule is same and check of left and right part of subtree
-            return dfs(root.left,subroot.left) and dfs(root.right,subroot.right)
-        
-        def solve(root,subroot):
-            
-            #if tree is empty return false
-            if not root:
-                return False
-            
-            #check if subtree exist in tree or not with root node same for both
-            if dfs(root,subroot):
-                return True
-            
-            #check of subtree in right and left subtree of tree
-            return solve(root.left,subroot) or solve(root.right,subroot)
-        return solve(root,subRoot)
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s: 
+            return False
+        if self.isSameTree(s, t): 
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
