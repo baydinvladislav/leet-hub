4a824b0e5c84cba339384e746d93a8750b82287b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-14 16:24:44 +0300

    Time: 41 ms (46.67%), Space: 13.8 MB (89.78%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,10 @@
-# Approach 3: Dynamic Programming
 class Solution:
     def climbStairs(self, n):
         if n == 1:
             return 1
 
         # create dp table
-        dp_table = [None for _ in range(n + 1)]
+        dp_table = [0 for _ in range(n + 1)]
 
         # fill db table with base cases
         dp_table[1] = 1
@@ -14,4 +13,4 @@ class Solution:
         for i in range(3, n + 1):
             dp_table[i] = dp_table[i - 1] + dp_table[i - 2]
 
-        return dp_table[n]
+        return max(dp_table)
