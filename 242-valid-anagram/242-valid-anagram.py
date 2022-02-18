55e8396e49cbda95ef5ffbe352463c595b1d1ab0
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-18 15:23:19 +0300

    Time: 56 ms (68.15%), Space: 14.5 MB (50.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,20 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        d1 , d2 = defaultdict(int), defaultdict(int)
+        for key in s:
+            d1[key] += 1
         
-        s = list(s)
-        t = list(t)
+        for key in t:
+            d2[key] += 1 
         
-        s.sort()
-        t.sort()
-       
-        i, j = 0, 0 
-        while i != len(s):
-            if s[i] != t[j]:
-                return False
-            i += 1 
-            j += 1
-        return True
+        return d1 == d2
             
-        
