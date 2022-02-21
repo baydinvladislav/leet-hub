536f5cc5840f05a2f92af164a7697952cb07a169
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-02-21 18:52:41 +0300

    Time: 109 ms (76.33%), Space: 18.5 MB (29.80%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        if not strs:
-            return [[""]]
-
         hash_map = defaultdict(list)
         for word in strs:
-            hash_map[tuple(sorted(word))].append(word)
-        return list(hash_map.values())
+            sorted_word = tuple(sorted(word))
+            hash_map[sorted_word].append(word)
+        return hash_map.values()
