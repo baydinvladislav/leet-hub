43c22019e8b246b4bd8f1e764a6634a7438b7049
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-09 12:38:14 +0300

    Time: 69 ms (18.72%), Space: 13.9 MB (88.73%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Approach 1: Recursion
+    # Time complexity: O(n + m)
+    # Space complexity: O(n + m)
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+
+        elif l2 is None:
+            return l1
+
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
