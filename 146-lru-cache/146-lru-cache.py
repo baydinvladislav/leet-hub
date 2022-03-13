7ff8805a8cdc07baa713436a2b8e7da2e5b956a8
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-13 21:19:04 +0300

    Time: 820 ms (91.33%), Space: 76.1 MB (37.43%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = OrderedDict()
+        
+    def get(self, key):
+        if key in self.storage:
+            self.storage.move_to_end(key)
+            return self.storage[key]
+        else:
+            return -1
+
+    def put(self, key, value):
+        if key in self.storage:
+            self.storage.move_to_end(key)
+        self.storage[key] = value
+        
+        if len(self.storage) > self.capacity:
+            self.storage.popitem(last = False)
+            
