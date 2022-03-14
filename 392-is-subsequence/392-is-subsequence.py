001d218b6c5860d365800ced33fd358d752e75a2
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-14 13:33:40 +0300

    Time: 36 ms (80.28%), Space: 14 MB (51.80%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,15 +1,13 @@
 class Solution:
-    # Approach 1: Divide and Conquer with Greedy
-    # Time Complexity: O(T)
-    # Space Complexity: O(T)
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-        if not t:
-            return False
+        left = 0
+        right = 0
+        while True:
+            if left == len(s):
+                return True
+            if right == len(t):
+                return False
 
-        if s[0] == t[0]:
-            s = s[1:]
-        t = t[1:]
-
-        return self.isSubsequence(s, t)
+            if s[left] == t[right]:
+                left += 1
+            right += 1
