ffcb8643e764a6742eb14740fcb72864be741028
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-20 17:33:43 +0300

    Time: 131 ms (53.64%), Space: 14.1 MB (75.93%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,27 +1,34 @@
 class Solution:
-    def find(self, nums1, s1, e1, nums2, s2, e2, k):
-        if e1 - s1 < 0:
-            return nums2[k + s2]
-        if e2 - s2 < 0:
-            return nums1[k + s1]
-        if k < 1:
-            return min(nums1[k + s1], nums2[k + s2])
-        ia, ib = (s1 + e1) // 2 , (s2 + e2) // 2
-        ma, mb = nums1[ia], nums2[ib]
-        if (ia - s1) + (ib - s2) < k:
-            if ma > mb:
-                return self.find(nums1, s1, e1, nums2, ib + 1, e2, k - (ib - s2) - 1)
-            else:
-                return self.find(nums1, ia + 1, e1, nums2, s2, e2, k - (ia - s1) - 1)
-        else:
-            if ma > mb:
-                return self.find(nums1, s1, ia - 1, nums2, s2, e2, k)
-            else:
-                return self.find(nums1, s1, e1, nums2, s2, ib - 1, k)
-    
     def findMedianSortedArrays(self, nums1, nums2):
-        l = len(nums1) + len(nums2)
-        if l % 2 == 1:
-            return self.find(nums1, 0, len(nums1) - 1, nums2, 0, len(nums2) - 1, l // 2)
-        else:
-            return (self.find(nums1, 0, len(nums1) - 1, nums2, 0, len(nums2) - 1, l // 2) + self.find(nums1, 0, len(nums1) - 1, nums2, 0, len(nums2) - 1, l // 2 - 1)) / 2.0
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        m, n = len(nums1), len(nums2)
+        
+        left_size = (m + n + 1) // 2
+        start = 0
+        end = m
+        is_even = ((m + n) % 2) == 0
+        while start <= end:
+            a_part = (start + end) // 2
+            b_part = left_size - a_part
+            
+            aleftmax = float("-inf") if a_part == 0 else nums1[a_part - 1]
+            arightmin = float("inf") if a_part == m else nums1[a_part]
+            bleftmax = float("-inf") if b_part == 0 else nums2[b_part - 1]
+            brightmin = float("inf") if b_part == n else nums2[b_part]
+            
+            if aleftmax <= brightmin and bleftmax <= arightmin:
+                if not is_even:
+                    return max(aleftmax, bleftmax)
+                else:
+                    return (max(aleftmax, bleftmax) + min(arightmin, brightmin))/ 2
+            elif aleftmax > brightmin:
+                end = a_part - 1
+            elif bleftmax > arightmin:
+                start = a_part + 1
