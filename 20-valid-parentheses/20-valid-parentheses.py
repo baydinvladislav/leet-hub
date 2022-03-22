a7ba4a884cceba3e72300979e52946089ebd1b3f
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-22 15:50:11 +0300

    Time: 59 ms (18.94%), Space: 13.8 MB (77.35%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,23 @@
 class Solution:
     def isValid(self, s):
-        d = {'(' : ')', '{':'}', '[':']'}
-        stack = deque()
-        
+        hash_map = {")": "(", "}": "{", "]": "["}
+        # stack to keep track of opening brackets
+        stack = []
+
         for char in s:
-            if char in d:
-                stack.append(char)
+            # if the character is a closing bracket
+            if char in hash_map:
+                # pop the topmost element from the stack, if it is not empty
+                # otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
+                # the mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if hash_map[char] != top_element:
+                    return False
             else:
-                if not stack:
-                    return False
-                
-                if d[stack.pop()] != char:
-                    return False
-            
+                # we have an opening bracket, simply push it onto the stack
+                stack.append(char)
+
+        # in the end, if the stack is empty, then we have a valid expression
+        # the stack won't be empty for cases like ((()
         return not stack
