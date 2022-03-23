d09fa513f6c5e7c58329623b5e426312f36b36b1
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-23 12:12:55 +0300

    Time: 60 ms (35.93%), Space: 14 MB (13.04%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,10 +6,10 @@ class Solution:
         if l1 is None:
             return l2
 
-        elif l2 is None:
+        if l2 is None:
             return l1
 
-        elif l1.val < l2.val:
+        if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
 
