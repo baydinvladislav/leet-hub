3f1b76518cb392afe9e51998e03c663e6115ab76
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-24 16:40:02 +0300

    Time: 36 ms (95.95%), Space: 13.9 MB (29.94%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -1,7 +1,7 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         students = 0
-        for i in range(len(startTime)):
+        for i in range(len(endTime)):
             if startTime[i] <= queryTime <= endTime[i]:
                 students += 1
         return students
