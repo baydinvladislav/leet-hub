9988fd02e96c8fc69841f284f6b98d556fadd1f9
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-24 14:23:54 +0300

    Time: 75 ms (9.01%), Space: 15.5 MB (61.75%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -1,5 +1,12 @@
 class Solution:
+    # Iterative
     def reverseList(self, head):
+        """
+        1. temporarily store the next node
+        2. reverse the current node
+        3. before we move to the next node, point previous to the current node
+        4. move on the next node
+        """
         prev = None
         current = head
         while current:
