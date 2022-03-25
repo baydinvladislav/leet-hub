5ab77a7f435aa86448396aa398d684973adfd8df
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-25 15:48:04 +0300

    Time: 44 ms (93.25%), Space: 16.6 MB (29.59%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -11,10 +11,9 @@ class Solution:
         def validate(node, low=-math.inf, high=math.inf):
             if not node:
                 return True
-
+            
             if node.val <= low or node.val >= high:
                 return False
-
-            return validate(node.right, node.val, high) and validate(node.left, low, node.val)
-
+            
+            return validate(node.left, low, node.val) and validate(node.right, node.val, high)
         return validate(root)
