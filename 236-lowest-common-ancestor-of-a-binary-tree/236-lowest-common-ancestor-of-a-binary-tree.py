b5882b69e882506b53a5bca4e56bdba378ec8be9
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-25 17:33:42 +0300

    Time: 91 ms (63.67%), Space: 26.3 MB (63.63%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,37 +1,30 @@
 class Solution:
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
 
     def lowestCommonAncestor(self, root, p, q):
-        # Stack for tree traversal
-        stack = [root]
+        def recurse_tree(current_node):
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
 
-        # Dictionary for parent pointers
-        parent = {root: None}
+            # Left Recursion
+            left = recurse_tree(current_node.left)
 
-        # Iterate until we find both the nodes p and q
-        while p not in parent or q not in parent:
+            # Right Recursion
+            right = recurse_tree(current_node.right)
 
-            node = stack.pop()
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
 
-            # While traversing the tree, keep saving the parent pointers.
-            if node.left:
-                parent[node.left] = node
-                stack.append(node.left)
+            # If any two of the three flags left, right or mid become True.
+            if mid + left + right >= 2:
+                self.ans = current_node
 
-            if node.right:
-                parent[node.right] = node
-                stack.append(node.right)
+            # Return True if either of the three bool values is True.
+            return mid or left or right
 
-        # Ancestors set() for node p.
-        ancestors = set()
-
-        # Process all ancestors for node p using parent pointers.
-        while p:
-            ancestors.add(p)
-            p = parent[p]
-
-        # The first ancestor of q which appears in
-        # p's ancestor set() is their lowest common ancestor.
-        while q not in ancestors:
-            q = parent[q]
-
-        return q
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
