9bef921118d666feb6596a658715395e7046932c
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-25 13:59:29 +0300

    Time: 48 ms (87.43%), Space: 14.4 MB (89.20%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,6 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_symbols = filter(lambda symbol: symbol.isalnum(), s)
-        lo_low_case = map(lambda symbol: symbol.lower(), filtered_symbols)
-        to_list = list(lo_low_case)
-        return to_list == to_list[::-1]
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
