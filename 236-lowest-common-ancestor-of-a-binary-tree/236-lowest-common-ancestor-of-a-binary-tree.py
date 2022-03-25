391791738317e1b8328769ac4161e3f148e27bd0
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-25 18:48:31 +0300

    Time: 140 ms (16.66%), Space: 26.2 MB (63.63%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,30 +1,23 @@
 class Solution:
     def __init__(self):
-        # Variable to store LCA node.
-        self.ans = None
+        self.answer = None
 
     def lowestCommonAncestor(self, root, p, q):
-        def recurse_tree(current_node):
-            # If reached the end of a branch, return False.
-            if not current_node:
+        def recursive(node):
+        
+            if not node:
                 return False
 
-            # Left Recursion
-            left = recurse_tree(current_node.left)
-
-            # Right Recursion
-            right = recurse_tree(current_node.right)
-
-            # If the current node is one of p or q
-            mid = current_node == p or current_node == q
-
-            # If any two of the three flags left, right or mid become True.
-            if mid + left + right >= 2:
-                self.ans = current_node
-
-            # Return True if either of the three bool values is True.
-            return mid or left or right
-
-        # Traverse the tree
-        recurse_tree(root)
-        return self.ans
+            left_branch = recursive(node.left)
+            right_branch = recursive(node.right)
+            
+            middle = node.val == p.val or node.val == q.val
+            
+            if int(left_branch) + int(right_branch) + middle >= 2:
+                self.answer = node
+            
+            return left_branch or right_branch or middle
+        
+        recursive(root)   
+        return self.answer
+            
