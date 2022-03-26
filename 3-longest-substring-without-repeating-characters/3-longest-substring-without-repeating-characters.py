7eaadc1d3e4e8e23a0fc209ee51649d0db61a44b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-26 21:00:12 +0300

    Time: 163 ms (23.29%), Space: 13.9 MB (94.53%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,48 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        arr = [0] * 128
+        arr = [0] *  128
         start = end = result = 0
         
         while end < len(s):
-            last_ch = s[end]
-            char_idx = ord(last_ch)
-            arr[char_idx] += 1
+            arr[ord(s[end])] += 1
             
-            while arr[ord(last_ch)] > 1:
-                first_ch = s[start]
-                char_idx = ord(first_ch)
-                arr[char_idx] -= 1
+            while arr[ord(s[end])] > 1:
+                arr[ord(s[start])] -= 1
                 start += 1
             
             result = max(result, end - start + 1)
             end += 1
             
         return result
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         max_length = 0
-        
-#         for i in range(len(s)):
-#             for j in range(i, len(s)):
-#                 if self.checker(i, j, s):
-#                     length = j - i + 1
-#                     max_length = max(length, max_length)
-#         return max_length
-    
-#     def checker(self, start, end, s):
-#         arr = [0] * 128
-        
-#         for i in range(start, end + 1):
-#             arr[ord(s[i])] += 1
-#             if arr[ord(s[i])] > 1:
-#                 return False
-#         return True
