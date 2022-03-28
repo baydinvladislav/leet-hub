236c35a061228f8e54c056aed5d4517e786e0236
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-28 15:32:01 +0300

    Time: 125 ms (53.10%), Space: 14 MB (71.72%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -10,10 +10,10 @@ class Solution:
             hash_map[ch] += 1
             
         for window_end in range(len(s2)):
-            last_symbol = s2[window_end]
-            if last_symbol in hash_map:
-                hash_map[last_symbol] -= 1
-                if hash_map[last_symbol] == 0:
+            right_symbol = s2[window_end]
+            if right_symbol in hash_map:
+                hash_map[right_symbol] -= 1
+                if hash_map[right_symbol] == 0:
                     matched += 1
             
             if matched == len(hash_map):
@@ -26,4 +26,6 @@ class Solution:
                         matched -= 1
                     hash_map[left_symbol] += 1
                 window_start += 1
+                
         return False
+                
