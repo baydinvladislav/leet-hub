8523cc7699e3ce14d21e4aa6a7d1d7c2391673b5
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-28 14:57:49 +0300

    Time: 101 ms (68.59%), Space: 14 MB (36.36%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,29 +2,28 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         window_start = 0
         matched = 0
-        hash = {}
+        hash_map = {}
         
-        for char in s1:
-            if char not in hash:
-                hash[char] = 0
-            hash[char] += 1
-        
+        for ch in s1:
+            if ch not in hash_map:
+                hash_map[ch] = 0
+            hash_map[ch] += 1
+            
         for window_end in range(len(s2)):
             last_symbol = s2[window_end]
-            if last_symbol in hash:
-                hash[last_symbol] -= 1
-                if hash[last_symbol] == 0:
+            if last_symbol in hash_map:
+                hash_map[last_symbol] -= 1
+                if hash_map[last_symbol] == 0:
                     matched += 1
             
-            if matched == len(hash):
+            if matched == len(hash_map):
                 return True
             
             if window_end >= len(s1) - 1:
-                first_symbol = s2[window_start]
-                if first_symbol in hash:
-                    if hash[first_symbol] == 0:
+                left_symbol = s2[window_start]
+                if left_symbol in hash_map:
+                    if hash_map[left_symbol] == 0:
                         matched -= 1
-                    hash[first_symbol] += 1
+                    hash_map[left_symbol] += 1
                 window_start += 1
-                
         return False
