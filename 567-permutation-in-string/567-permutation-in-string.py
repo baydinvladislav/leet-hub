2bebb67cfe4a0d18c0f3541e35b3097d1aec8caf
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-28 14:51:33 +0300

    Time: 122 ms (54.69%), Space: 14 MB (36.36%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,28 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        window_start, matched, frequency_map = 0, 0, dict()
-
-        for ch in s1:
-            if ch not in frequency_map:
-                frequency_map[ch] = 0
-            frequency_map[ch] += 1
-
+        window_start = 0
+        matched = 0
+        hash = {}
+        
+        for char in s1:
+            if char not in hash:
+                hash[char] = 0
+            hash[char] += 1
+        
         for window_end in range(len(s2)):
-            right_char = s2[window_end]
-            if right_char in frequency_map:
-                frequency_map[right_char] -= 1
-                if frequency_map[right_char] == 0:
+            last_symbol = s2[window_end]
+            if last_symbol in hash:
+                hash[last_symbol] -= 1
+                if hash[last_symbol] == 0:
                     matched += 1
-
-            if matched == len(frequency_map):
+            
+            if matched == len(hash):
                 return True
-
+            
             if window_end >= len(s1) - 1:
-                left_char = s2[window_start]
-                if left_char in frequency_map:
-                    if frequency_map[left_char] == 0:
+                first_symbol = s2[window_start]
+                if first_symbol in hash:
+                    if hash[first_symbol] == 0:
                         matched -= 1
-                    frequency_map[left_char] += 1
+                    hash[first_symbol] += 1
                 window_start += 1
-
+                
         return False
