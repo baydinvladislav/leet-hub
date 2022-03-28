6050be3d2657bbcb18edbe788ff050f5921b535b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-28 16:28:17 +0300

    Time: 831 ms (12.11%), Space: 14.8 MB (40.13%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -1,18 +1,18 @@
 class Solution:
-    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+    def lengthOfLongestSubstringTwoDistinct(self, s: 'str') -> 'int':
         window_start = 0
-        result = float('-inf')
+        max_length = float('-inf')
         hash_map = {}
-        
+
         for window_end in range(len(s)):
             if s[window_end] not in hash_map:
                 hash_map[s[window_end]] = 0
             hash_map[s[window_end]] += 1
-            
+
             while len(hash_map) > 2:
                 hash_map[s[window_start]] -= 1
                 if hash_map[s[window_start]] == 0:
                     del hash_map[s[window_start]]
                 window_start += 1
-            result = max(result, window_end - window_start + 1)
-        return result
+            max_length = max(max_length, window_end - window_start + 1)
+        return max_length
