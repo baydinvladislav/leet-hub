43369ed0b03dd6a8e1d5b77c2075e20a33e0d212
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-29 16:02:54 +0300

    Time: 64 ms (83.45%), Space: 13.9 MB (78.37%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,19 +1,25 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        left = result = 0
-        
-        while left < len(chars):
-            right = left
-            while right < len(chars) and chars[left] == chars[right]:
-                right += 1
-                
-            chars[result] = chars[left]
+        first_pointer = result = 0
+
+        while first_pointer < len(chars):
+            second_pointer = first_pointer
+            while second_pointer < len(chars) and chars[second_pointer] == chars[first_pointer]:
+                second_pointer += 1
+
+            chars[result] = chars[first_pointer]
             result += 1
-            diff = right - left
-            if diff > 1:
-                for number in str(diff):
-                    chars[result] = number
-                    result += 1
             
-            left = right
+            diff = second_pointer - first_pointer
+            if diff > 1:
+                # multiple characters like 10, 12, etc
+                for digit in str(diff):
+                    chars[result] = digit
+                    result += 1
+
+            first_pointer = second_pointer
+
+        # you also can return a string result
+        chars = ''.join(chars[:result])
+
         return result
