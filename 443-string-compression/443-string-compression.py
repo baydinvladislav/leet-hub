d195a798830617efddff6608c95259c627222d8d
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-29 16:02:19 +0300

    Time: 64 ms (83.45%), Space: 14.1 MB (37.21%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,6 +1,6 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        left = result = 0 
+        left = result = 0
         
         while left < len(chars):
             right = left
@@ -9,14 +9,11 @@ class Solution:
                 
             chars[result] = chars[left]
             result += 1
-
             diff = right - left
             if diff > 1:
                 for number in str(diff):
                     chars[result] = number
                     result += 1
-
+            
             left = right
-                
         return result
-                
