755db5a6fb8ce569af670dcc9223644cd28c827b
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-03-31 20:56:16 +0300

    Time: 172 ms (43.60%), Space: 19.7 MB (37.37%) - LeetHub

--- 356-line-reflection/356-line-reflection.py
+++ 356-line-reflection/356-line-reflection.py
@@ -4,5 +4,8 @@ class Solution:
         minx, _ = min(points, key=lambda x: x[0])
         maxx, _ = max(points, key=lambda x: x[0])
         rx = (minx + maxx) / 2
-
-        return all((2 * rx - x, y) in points for x, y in points)
+        
+        result = []
+        for x, y in points:
+            result.append((2 * rx - x, y) in points)
+        return all(result) 
