7d35b852d9522c07541ec27d078d73d1af638aec
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-04-01 14:23:51 +0300

    Time: 136 ms (75.09%), Space: 19.6 MB (57.54%) - LeetHub

--- 356-line-reflection/356-line-reflection.py
+++ 356-line-reflection/356-line-reflection.py
@@ -1,13 +1,17 @@
 class Solution:
     def isReflected(self, points: List[List[int]]) -> bool:
         points = set(map(tuple, points))
-        minx, _ = min(points, key=lambda x: x[0])
-        maxx, _ = max(points, key=lambda x: x[0])
-        rx = (minx + maxx) / 2
-
+        
+        point_min_x = min(points, key=lambda x: x[0])
+        point_max_x = max(points, key=lambda x: x[0])
+        
+        middle_line = (point_min_x[0] + point_max_x[0]) / 2
+        
         result = []
         for x, y in points:
-            point = (2 * rx - x, y)
-            result.append(point in points)
-
+            mirror_point = (2 * middle_line - x, y)
+            result.append(mirror_point in points)
+            
         return all(result)
+            
+            
