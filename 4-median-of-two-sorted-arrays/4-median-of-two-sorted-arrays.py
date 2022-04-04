f4841d10ff4988f2659a97565f20aeeee35b5986
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-04-04 17:29:29 +0300

    Time: 132 ms (51.34%), Space: 14.3 MB (7.07%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,31 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-
-        m, n = len(nums1), len(nums2)
-
-        left_size = (m + n + 1) // 2
-        start, end = 0, m
-        is_even = ((m + n) % 2) == 0
-        while start <= end:
-            a_part = (start + end) // 2
-            b_part = left_size - a_part
-
-            aleftmax = float("-inf") if a_part == 0 else nums1[a_part - 1]
-            arightmin = float("inf") if a_part == m else nums1[a_part]
-            
-            bleftmax = float("-inf") if b_part == 0 else nums2[b_part - 1]
-            brightmin = float("inf") if b_part == n else nums2[b_part]
-
-            if aleftmax <= brightmin and bleftmax <= arightmin:
-                if not is_even:
-                    return max(aleftmax, bleftmax)
-                else:
-                    return (max(aleftmax, bleftmax) + min(arightmin, brightmin)) / 2
-            
-            elif aleftmax > brightmin:
-                end = a_part - 1
-                
-            elif bleftmax > arightmin:
-                start = a_part + 1
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2) 
+        half = total // 2
+        
+        if len(B) < len(A):
+            A, B = B, A
+        
+        left, right = 0, len(A) - 1
+        while True:
+            i = (left + right) // 2
+            j = half - i - 2
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i + 1] if (i + 1) < len(A) else float('inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j + 1] if (j + 1) < len(B) else float('inf')
+        
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                right = i - 1
+            else:
+                left = i + 1
