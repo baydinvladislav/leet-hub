eb9802b2c940ef98c5681c16be03cded9845540d
Vladislav Baydin <baydinvladislav@yandex.ru>
2022-06-28 15:21:21 +0300

    Time: 9184 ms (14.30%), Space: 14 MB (50.50%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,27 +1,32 @@
+from typing import List
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(x, y, index):
             if index == len(word):
                 return True
-            
-            if (not 0 <= x < len(board) or 
-                not 0 <= y < len(board[0]) or 
-                board[x][y] == '#' or 
-                board[x][y] != word[index]):
+
+            if (
+                not 0 <= x < len(board) or
+                not 0 <= y < len(board[0]) or
+                board[x][y] == '#' or
+                board[x][y] != word[index]
+            ):
                 return False
-            
+
             curr_letter = board[x][y]
             board[x][y] = '#'
             
-            top = dfs(x - 1, y, index + 1)
-            bottom = dfs(x + 1, y, index + 1)
-            left = dfs(x, y - 1, index + 1)
-            right = dfs(x, y + 1, index + 1)
-            
+            is_result = False
+            for x_offset, y_offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                if dfs(x + x_offset, y + y_offset, index + 1):
+                    is_result = True
+
             board[x][y] = curr_letter
-            
-            return top or bottom or left or right
-    
+
+            return is_result
+
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if dfs(i, j, 0):
